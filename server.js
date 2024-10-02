f339e44858d3a9af6f842d900215b5ee30aefcbb
Aayush Mall <aayush@Aayushs-MacBook-Air.local>
2024-10-02 11:23:53 +0530

    Added cross origin request

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config({ path: '.env' })
 const cors = require('cors');
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 connectDB();
 app.use(express.json());
 app.use(cors());
